85ba3a347f9b6a63ba13ede50fd23ea636dd362e
Abyssion <alexis@lattard.fr>
2018-11-30 09:30:35 +0100

    Début implementation ProdConsBuffer

--- src/jus/poc/prodcons/v1/ProdConsBuffer.java
+++ src/jus/poc/prodcons/v1/ProdConsBuffer.java
@@ -1,5 +1,32 @@
 package jus.poc.prodcons.v1;
 
-public class ProdConsBuffer {
+import java.util.ArrayList;
+
+public class ProdConsBuffer implements IProdConsBuffer {
+
+	private static final int sizeMax = 20;
+	
+	private ArrayList<Message> buffer;
+	
+	public ProdConsBuffer() {
+		buffer = new ArrayList<Message>();
+	}
+
+	public void put(Message m) throws InterruptedException{
+		if(buffer.size() < sizeMax) {
+			buffer.add(m);
+		}
+	}
+
+	public Message get() throws InterruptedException {		
+		if(buffer.size() <= 0) {
+			throw new InterruptedException();
+		}
+		return buffer.remove(buffer.size());
+	}
+
+	public int nmsg() {
+		return buffer.size();
+	}
 
 }
